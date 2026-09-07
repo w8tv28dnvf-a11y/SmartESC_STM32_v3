1c47b267ecdf91831024c990b7d2694264ac15c9
w8tv28dnvf-a11y <w8tv28dnvf@privaterelay.appleid.com>
2026-09-07 03:31:40 +0200

    Update config.h

--- Core/Inc/config.h
+++ Core/Inc/config.h
@@ -4,9 +4,9 @@
 #define BRAKEMAX 100
 
 // speed limits for invividual modes in kph
-#define SPEEDLIMIT_ECO 6
-#define SPEEDLIMIT_NORMAL 20
-#define SPEEDLIMIT_SPORT 50
+#define SPEEDLIMIT_ECO 20
+#define SPEEDLIMIT_NORMAL 35
+#define SPEEDLIMIT_SPORT 70
 
 // motor current limits for invividual modes in mA
 // note that hacked firmware allows up to 55amps motor phase current
@@ -18,7 +18,7 @@
 #define REGEN_MAX_CURRENT 10000
 
 // maximum current for field weakening in mA
-#define FIELD_WEAKNING_CURRENT_MAX 0 //max id
+#define FIELD_WEAKNING_CURRENT_MAX 30 //max id
 
 // ADC channels
 #define ADC_VOLTAGE 0
